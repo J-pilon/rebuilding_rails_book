86a4fa91c46d4ce670bd2de504210f87fd52e4d9
Josiah <josiah.pilon@gmail.com>
2025-03-30 12:38:11 -0700

    test: application responds to GET "/" with status ok

--- rulers/spec/rulers_spec.rb
+++ rulers/spec/rulers_spec.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+class TestApp < Rulers::App
+end
+
 RSpec.describe Rulers do
   it "has a version number" do
     expect(Rulers::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  def app
+    TestApp.new
+  end
+
+  it "GET / responds with status ok" do
+    get "/"
+    body = last_response.body
+    expect(last_response.ok?).to be(true)
+    expect(body).to be("Hello, World!")
   end
 end
