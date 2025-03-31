94b1f80ed94588a7bf91229d05ee34486224c511
Josiah <josiah.pilon@gmail.com>
2025-03-30 20:10:26 -0700

    feat: dynamically route requests to controller and action

--- best_quotes/config/app.rb
+++ best_quotes/config/app.rb
@@ -1,4 +1,7 @@
 require "rulers"
+$LOAD_PATH << File.join(__dir__, "..", "app", "controllers")
+require "quotes_controller"
+require "home_controller"
 
 module BestQuotes
   class App < Rulers::App
--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,17 +1,56 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+# require "rulers/routing"
 require "rulers/array"
 
 module Rulers
   class App
     def call(env)
-      `echo debug > debug.txt`
-      [
-        200,
-        {"Content-Type" => "text/html"},
-        ["Hello, World!"]
-      ]
+      path_info = env["PATH_INFO"]
+
+      if favicon_path?(path_info)
+        return [404, {"Content-Type": "text/html"}, []]
+      end
+
+      if root_path?(path_info)
+        return [ 302, {"Content-Type" => "text/html", "Location" => "/quotes/a_quote"}, [] ]
+      end
+
+      begin
+        klass, act = get_cont_and_act(env)
+        cont = klass.new(env)
+        text = cont.send(act)
+        [ 200, {"Content-Type" => "text/html"}, [text] ]
+      rescue => e
+        [500, {"Content-Type" => "text/html"}, ["Error 500: #{e}"]]
+      end
+    end
+
+    def get_cont_and_act(env)
+      _, cont, act, after = env["PATH_INFO"].split("/", 4)
+
+      cont = cont.capitalize
+      cont += "Controller"
+      [Object.const_get(cont), act]
+    end
+
+    def favicon_path?(path)
+      path == "/favicon.ico"
+    end
+
+    def root_path?(path)
+      path == "/"
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
