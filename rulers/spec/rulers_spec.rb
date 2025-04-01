27461a2f2f709cbb13d557d1d5a77acc363d00fb
Josiah <josiah.pilon@gmail.com>
2025-03-31 20:52:11 -0700

    test: controller actions

--- rulers/spec/rulers_spec.rb
+++ rulers/spec/rulers_spec.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
+class TestController < Rulers::Controller
+  def index
+    "Hello World!"
+  end
+end
+
 class TestApp < Rulers::App
+  def get_cont_and_act(env)
+    [TestController, "index"]
+  end
 end
 
 RSpec.describe Rulers do
@@ -14,8 +23,22 @@ RSpec.describe Rulers do
 
   it "GET / responds with status ok" do
     get "/"
+    expect(last_response.redirect?).to be(true)
+  end
+
+  it "GET / responds with status redirect" do
+    get "/"
+    expect(last_response.location).to be("/quotes/a_quote")
+  end
+
+  it "GET /favicon.ico responds with status not_found" do
+    get "/favicon.ico"
+    expect(last_response.not_found?).to be(true)
+  end
+
+  it "GET /quotes/a_quote" do
+    get "/quotes/a_quote"
     body = last_response.body
-    expect(last_response.ok?).to be(true)
-    expect(body).to be("Hello, World!")
+    expect(body).to eq("Hello World!")
   end
 end
